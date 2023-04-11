2272544da166228bcdf7144820f1376c62eaefc9
nachomuzzu <nachomuzzupappa1337@gmail.com>
2023-04-11 18:57:31 -0300

    Entregable 3

---
+++ Entregable 3/src/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import path from "path";
+import ProductManager from "./ProductManager.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+const productManager = new ProductManager(
+  path.resolve(process.cwd(), "public", "products.json")
+);
+
+app.get("/", (req, res) => {
+  res.send(" Bienvenido!");
+});
+
+app.get("/productos", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    const limit = req.query.limit;
+    let limitedProducts;
+    if (limit) {
+      limitedProducts = products.slice(0, limit);
+    }
+    res.send(limitedProducts || products);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Crear ruta
+app.post("/productos", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    const newProduct = req.body;
+    await productManager.addProduct(products, newProduct);
+    res.send(newProduct);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Iniciar server
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
